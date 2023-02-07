1f4635435218c00b7f48b55a62f041ec4b0b577b
tomato645 <rajiohousou@gmail.com>
2023-02-07 21:44:59 +0900

    fix the mistakes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 
-audio_bytes = audio_recorder()
+audio_bytes = audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=41_000)
 if audio_bytes:
-    audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=41_000)
+    st.audio(audio_bytes, format="audio/wav")
