fda8c13271ad1cc3dd6bc79777421e4384af27a1
tomato645 <rajiohousou@gmail.com>
2023-02-09 05:25:24 +0900

    short time

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from audio_recorder_streamlit import audio_recorder
 
 audio_bytes = audio_recorder(
   energy_threshold=(-1.0, 1.0),
-  pause_threshold=3.0,
+  pause_threshold=0.1,
 )
 
 if audio_bytes:
