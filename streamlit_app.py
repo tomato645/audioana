32873e8084135a170830add40e9b06b418a4a05b
tomato645 <rajiohousou@gmail.com>
2023-02-09 05:33:32 +0900

    by chatgpt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,32 @@
 import streamlit as st
-from audio_recorder_streamlit import audio_recorder
+import numpy as np
+import sounddevice as sd
+import matplotlib.pyplot as plt
 
-audio_bytes = audio_recorder(
-  energy_threshold=(-1.0, 1.0),
-  pause_threshold=0.1,
-)
+fs = 44100  # sample rate
+duration = 5  # recording duration
 
-if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
+
+def get_note(frequency):
+    notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    reference_note = 440
+    semitones = 12 * (np.log2(frequency / reference_note))
+    note_index = int(semitones) % 12
+    return notes[note_index]
+
+
+def audio_callback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    # Get the most prominent frequency from the audio
+    frequency = np.abs(np.fft.rfft(indata[:, 0]))
+    frequency = np.argmax(frequency) * fs / (2 * (indata.shape[0] - 1))
+    note = get_note(frequency)
+    print(f'Note: {note}')
+
+
+with sd.InputStream(callback=audio_callback, blocksize=int(fs * duration),
+                    samplerate=fs, channels=1):
+    print('Recording...')
+    sd.sleep(duration * 1000)
+    print('Done.')
